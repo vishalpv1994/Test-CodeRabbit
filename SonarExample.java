5b90143870165d935ff3b7aa2e62a6fec84ccfff
vishalpv1994 <vishal.pv94@gmail.com>
2024-07-07 20:38:14 +0530

    updated list method

--- SonarExample.java
+++ SonarExample.java
@@ -24,7 +24,6 @@ public class SonarExample {
     
     public void resourceLeak() {
         List<String> list = new ArrayList<>();
-        // Missing list.close() or using try-with-resources
     }
 
    
