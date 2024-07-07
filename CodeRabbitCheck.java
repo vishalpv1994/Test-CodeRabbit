b37b5af9d7a30476fd84af989f262da21223ed2a
vishalpv1994 <vishal.pv94@gmail.com>
2024-07-07 12:35:23 +0530

    Add files via upload
    new file added

---
+++ CodeRabbitCheck.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeRabbitCheck {
+
+    private static final String CONSTANT_VALUE = "constant";
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+
+        String unusedVariable = "This variable is unused.";
+   
+        String nullString = null;
+        if (nullString.equals("test")) {
+            System.out.println("This won't be reached.");
+        }
+
+        for (String item : list) {
+            System.out.println(item);
+        }
+        for (String item : list) {
+            System.out.println(item);
+        }
+
+        String result = "";
+        for (String item : list) {
+            result += item + " ";
+        }
+        System.out.println("Result: " + result);
+
+        System.out.println(CONSTANT_VALUE);
+
+      
+        try {
+            // Some code that might throw an exception
+        } catch (Exception e) {
+            // Empty catch block
+        }
+
+        unusedPrivateMethod();
+
+    }
+
+    private static void unusedPrivateMethod() {
+        // This method is unused
+    }
+}
